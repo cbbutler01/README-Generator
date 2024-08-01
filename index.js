52689c00646977bcf7ef53272750dd3c00eb42db
cbbutler01 <clarenceb.butleriii@gmail.com>
2024-08-01 16:32:05 -0600

    added base functionality for README generator

---
+++ index.js
@@ -0,0 +1,79 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const path = require('path')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
+// TODO: Create an array of questions for user input
+const questions = [
+{
+    type: 'input',
+    message: 'what is your email address?',
+    name: 'Email',
+},
+{
+    type: 'input',
+    message: 'How can people reach out to you for more information on your project?',
+    name: 'Contact',
+},
+{
+    type: 'input',
+    message: 'What is your Github username?',
+    name: 'username',
+}, 
+{
+    type: 'input',
+    message: 'What would you like to title your project?',
+    name: 'Title',
+},
+{
+    type: 'input',
+    message: 'How would you describe your project?',
+    name: 'Description',
+
+},
+{
+    type: 'input',
+    message: 'what are the instruction for others users to install your project?',
+    name: 'InstallationInstructions',
+},
+{
+    type: 'input',
+    message: 'How should your project be used/What should it be used for?',
+    name: 'UsageInformation',
+},
+{
+    type: 'input',
+    message: 'How could other users contribute to your project?',
+    name: 'ContributionGuidlines',
+},
+{
+    type: 'input',
+    message: 'How should your project be tested for integrations?',
+    name: 'TestInstructions',
+},
+{
+    type: 'list',
+    message: 'What license would you like to use for your project?',
+    name: 'license',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD3', 'None'],
+},
+];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    try{
+    fs.writeFileSync(fileName, data);
+    console.log(`Successfully created ${fileName}`);
+    } catch (err) {
+        console.error(`Error writing ${fileName}: ${err}`)
+    }
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.createPromptModule(questions).then((userAnswers) => {
+        writeToFile('ReadMe.md', generateMarkdown(userAnswers))
+    })
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,68 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if(!license) {
+    return "";
+  }
+  return `![License](https://img.shields.io/badge/License-${license}-brightgreen)`;
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if(!license) {
+    return ""
+  }
+  return `[License](https://opensource.org/licenses/${license})`
+};
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if(!license) {
+    return "This project is not licensed.";
+  }
+  renderLicenseLink()
+  return `Licensed under [${license}](https://opensource.org/licenses/${license}) license.`
+};
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
+  return `
+  # ${data.title}
+
+  ## ${Description}
+  
+  ## Table of Contents
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[Contributing](#contributing)
+    -[Tests](#tests)
+    -[Questions](#questions)
+    -[License](#license)
+
+  ##Installation
+  ${data.InstallationIndtructions}
+
+  ##Usage
+  ${data.UsageInformation}
+
+  ##Contributing
+  ${data.ContrbutionGuidlines}
+
+  ##Tests
+  ${data.TestInstructions}
+
+  ##Questions
+  For any questions about the project, contact [${data.username}](https://github.com/${data.username}) or Email me at ${data.Email}.
+
+  ## License
+  ${licenseBadge}
+  ${licenseSection}
+`;
+}
+
+module.exports = generateMarkdown;
